e0305ccac0df3e0a56b98af293e170af0f27a5a6
Sara1502 <moreirasaragds@gmail.com>
2026-01-21 11:53:39 -0300

    Filtrar livros indisponiveis

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -2,19 +2,29 @@ const elementoParaInserirLivros = document.getElementById('livros')
 
 function exibirOsLivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML = ''
-    listaDeLivros.forEach(livros => {
+    listaDeLivros.forEach(livro => {
+        //let disponibilidade = verificarDisponibilidadeDoLivro(livro)
+        let disponibilidade = livro.quantidade > 0 ? 'livro_imagens' : 'livro_imagens indisponivel'
         elementoParaInserirLivros.innerHTML += `
             <div class="livro">
-                <img class="livro__imagens" src="${livros.imagem}" alt="${livros.alt}" />
+                <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
                 <h2 class="livro__titulo">
-                   ${livros.titulo}
+                   ${livro.titulo}
                 </h2>
-                <p class="livro__descricao">${livros.autor}</p>
-                <p class="livro__preco" id="preco">R$${livros.preco.toFixed(2)}</p>
+                <p class="livro__descricao">${livro.autor}</p>
+                <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
                 <div class="tags">
-                    <span class="tag">${livros.categoria}</span>
+                    <span class="tag">${livro.categoria}</span>
                 </div>
             </div>
         `
     });
 } 
+
+//function verificarDisponibilidadeDoLivro(livro) {
+//    if (livro.quantidade > 0) {
+//        return 'livro_imagens'
+//    } else {
+//        return 'livros_imagens indisponivel'
+//    }
+//}
